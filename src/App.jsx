81977fe7e21fa6d21d7bad32e64be8eeb78d99fa
JeanBionde <225014.aluno@iserj.edu.br>
2024-08-08 16:42:53 -0300

    estrutura inicial do projeto

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>teste vite</h1>
+      </div>
+      <p className="read-the-docs">
+        qualquer coisa
+      </p>
+    </>
+  )
+}
+
+export default App
